b316873bec0122fe6d36001a76805b118690af0b
Chapelle Nathan <nathanchapelle76@gmail.com>
2021-10-11 01:04:49 +0200

    query is now a prepared query

--- form.php
+++ form.php
@@ -25,8 +25,13 @@ if(!empty($data)) {
         $firstname = $data['firstname'];
         $lastname = $data['lastname'];
 
-        $query = "INSERT INTO friend(firstname, lastname) VALUES ('$firstname', '$lastname')";
-        $pdo->exec($query);
+        $query = 'INSERT INTO  friend (firstname, lastname) VALUES(:firstname, :lastname)';
+        $statement = $pdo->prepare($query);
+
+        $statement->bindValue(':firstname', $firstname, \PDO::PARAM_STR);
+        $statement->bindValue(':lastname', $lastname, \PDO::PARAM_STR);
+
+        $statement->execute();
         
         header('Location: index.php');
     }
